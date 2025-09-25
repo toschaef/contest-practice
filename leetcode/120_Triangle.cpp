fb5954de2f64371f00dea6ebfcd4474d83b04175
toschaef <notthomasschaefer@gmail.com>
2025-09-25 11:56:09 -0500

    daily

---
+++ leetcode/120_Triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i-1][0];
+                } else if (j == i) {
+                    triangle[i][j] += triangle[i-1][i-1];
+                } else {
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+            }
+        }
+        return *min_element(triangle.back().begin(), triangle.back().end());
+    }
+};
